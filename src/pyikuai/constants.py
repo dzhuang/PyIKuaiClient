9ad6a988871f7e22d13443ae2f46a505123eeb52
dzhuang <dzhuang.scut@gmail.com>
2024-02-13 22:38:43 +0800

    Added api for acl_l7.

--- src/pyikuai/constants.py
+++ src/pyikuai/constants.py
@@ -1,8 +1,80 @@
 JSON_RESPONSE_RESULT = "Result"
 JSON_RESPONSE_ERRMSG = "ErrMsg"
 JSON_RESPONSE_ERRMSG_SUCCESS = "Success"
+JSON_RESPONSE_DATA = "Data"
 
 
 class json_result_code:  # noqa
     code_10001 = 10001
     code_10000 = 10000
+    code_30000 = 30000
+
+
+class rp_key:  # noqa
+    action = "action"
+    func_name = "func_name"
+    param = "param"
+
+
+class rp_action:  # noqa
+    show = "show"
+    add = "add"
+    edit = "edit"
+    delete = "del"
+    up = "up"
+    down = "down"
+
+
+class rp_func_name:  # noqa
+    macgroup = "macgroup"
+    acl_l7 = "acl_l7"
+
+
+class QueryRPParam:
+    def __init__(
+            self, param_type: list | None = None,
+            limit=None, order_by=None, order_param=None):
+        """
+        :param param_type: list
+        :param limit: list of int
+        :param order_by: string
+        :param order_param: string
+        """
+        self.param_type = param_type or ["data", "total"]
+        self.limit = limit or [0, 100]
+        self.order_by = order_by or ""
+        self.order_param = order_param or ""
+
+    def as_dict(self):
+        return {
+            "TYPE": ",".join(self.param_type),
+            "limit": ",".join(map(str, self.limit)),
+            "ORDER_BY": self.order_by,
+            "ORDER": self.order_param
+        }
+
+
+class mac_group_param:  # noqa
+    id = "id"
+    group_name = "group_name"
+    addr_pool = "addr_pool"
+    comment = "comment"
+    newRow = "newRow"
+
+
+class acl_l7_param:  # noqa
+    action = "action"
+    app_proto = "app_proto"
+    comment = "comment"
+    dst_addr = "dst_addr"
+    enabled = "enabled"
+    id = "id"
+    prio = "prio"
+    src_addr = "src_addr"
+    time = "time"
+    week = "week"
+
+
+class acl_l7_param_action:  # noqa
+    drop = "drop"
+    accept = "accept"
--- src/pyikuai/core.py
+++ src/pyikuai/core.py
@@ -1,12 +1,15 @@
 import base64
 import hashlib
 import json
-from urllib.parse import urljoin
+from urllib.parse import quote, urljoin
 
 import requests
 
-from .constants import (JSON_RESPONSE_ERRMSG, JSON_RESPONSE_ERRMSG_SUCCESS,
-                        JSON_RESPONSE_RESULT, json_result_code)
+from .constants import (JSON_RESPONSE_DATA, JSON_RESPONSE_ERRMSG,
+                        JSON_RESPONSE_ERRMSG_SUCCESS, JSON_RESPONSE_RESULT,
+                        QueryRPParam, acl_l7_param, acl_l7_param_action,
+                        json_result_code, mac_group_param, rp_action,
+                        rp_func_name, rp_key)
 from .exceptions import AuthenticationError, RequestError, RouterAPIError
 
 
@@ -34,8 +37,7 @@ class IKuai:  # noqa
             'remember_password': "",
             'username': self._username
         }
-        if self._session is None:
-            self._session = requests.session()
+        self._session = requests.session()
 
         response = (
             self._session.post(f'{self.base_url}/Action/login', json=login_info))
@@ -54,12 +56,14 @@ class IKuai:  # noqa
 
     def exec(self, func, action, param, ensure_success=True):
         payload = {
-            "func_name": func,
-            "action": action,
-            "param": param
+            rp_key.func_name: func,
+            rp_key.action: action,
+            rp_key.param: param
         }
         response = self.session.post(
-            urljoin(self.base_url, "/Action/call"), json=payload)
+            urljoin(self.base_url, "/Action/call"), json=payload, headers={
+                "Content-Type": 'application/json'
+            })
         if response.status_code == 200:
             try:
                 content = response.json()
@@ -69,14 +73,166 @@ class IKuai:  # noqa
                 if content[JSON_RESPONSE_ERRMSG] == JSON_RESPONSE_ERRMSG_SUCCESS:
                     return content
                 else:
+                    if content[JSON_RESPONSE_ERRMSG] == "no login authentication":
+                        self._session = None
+                        return self.exec(func, action, param, ensure_success)
                     raise RouterAPIError(
                         f"API result error: '{content[JSON_RESPONSE_ERRMSG]}': "
                         f"{repr(content)}"
                     )
-
             except json.JSONDecodeError:
+                # the response is not a valid json
+                if 'sending to kernel ...' in response.content.decode():
+                    return json.loads(
+                        response.content.decode().replace(
+                            "sending to kernel ...", "").replace("\n", ""))
+
                 raise RequestError(
                     f"Error parsing response: {response.content.decode()}")
 
         raise RequestError(
             f"Request failed with response status code: {response.status_code}")
+
+    # {{{ mac group CRUD
+
+    def add_mac_group(self, group_name, addr_pools, comments=None):
+        comments = comments or []
+        return self.exec(
+            func=rp_func_name.macgroup,
+            action=rp_action.add,
+            param={
+                mac_group_param.newRow: True,
+                mac_group_param.group_name: group_name,
+                mac_group_param.addr_pool: ",".join(addr_pools),
+                mac_group_param.comment: (f",{quote(' ')}".join(comments))
+            }
+        )
+
+    def get_mac_groups(self):
+        result = self.exec(
+            func=rp_func_name.macgroup,
+            action=rp_action.show,
+            param=QueryRPParam().as_dict()
+        )
+        return result[JSON_RESPONSE_DATA]
+
+    def edit_mac_group(self, group_id, group_name, addr_pools, comments=None):
+        comments = comments or []
+        return self.exec(
+            func=rp_func_name.macgroup,
+            action=rp_action.edit,
+            param={
+                mac_group_param.id: group_id,
+                mac_group_param.group_name: group_name,
+                mac_group_param.addr_pool: ",".join(addr_pools),
+                mac_group_param.comment: (f",{quote(' ')}".join(comments))
+            }
+        )
+
+    def del_mac_group(self, group_id):
+        return self.exec(
+            func=rp_func_name.macgroup,
+            action=rp_action.delete,
+            param={
+                mac_group_param.id: group_id,
+            }
+        )
+
+    # }}}
+
+    # {{{ acl_l7 CRUD
+    # 行为管控 之 应用协议控制
+
+    def _get_acl_l7_param(
+            self, comment, src_addrs: list, action,
+            dst_addrs: list | None=None,
+            prio=32, app_protos=None, enabled=True, time="00:00-23:59",
+            week="1234567"):
+        app_protos = app_protos or []
+        enabled = "yes" if enabled else "no"
+        comment = comment.replace(" ", quote(" "))
+        assert action in [acl_l7_param_action.accept, acl_l7_param_action.drop]
+
+        dst_addrs = dst_addrs or []
+        dst_addr = ",".join(dst_addrs)
+
+        src_addr = ",".join(src_addrs)
+
+        param = {
+            acl_l7_param.action: action,
+            acl_l7_param.app_proto: ",".join(app_protos),
+            acl_l7_param.comment: comment,
+            acl_l7_param.dst_addr: dst_addr or "",
+            acl_l7_param.enabled: enabled,
+            acl_l7_param.prio: prio,
+            acl_l7_param.src_addr: src_addr,
+            acl_l7_param.time: time,
+            acl_l7_param.week: week
+        }
+        return param
+
+    def add_acl_l7(self, comment, src_addrs, action, dst_addrs=None,
+                   prio=32, app_protos=None, enabled=True, time="00:00-23:59",
+                   week="1234567"):
+
+        param = self._get_acl_l7_param(
+            comment, src_addrs, action, dst_addrs, prio,
+            app_protos, enabled, time, week)
+
+        return self.exec(
+            func=rp_func_name.acl_l7,
+            action=rp_action.add,
+            param=param
+        )
+
+    def get_acl_l7(self):
+        result = self.exec(
+            func=rp_func_name.acl_l7,
+            action=rp_action.show,
+            param=QueryRPParam().as_dict()
+        )
+        return result[JSON_RESPONSE_DATA]
+
+    def edit_acl_l7(self, acl_l7_id, comment, src_addrs, action, dst_addrs=None,
+                    prio=32, app_protos=None, enabled=True, time="00:00-23:59",
+                    week="1234567"):
+        param = self._get_acl_l7_param(
+            comment, src_addrs, action, dst_addrs, prio,
+            app_protos, enabled, time, week)
+
+        param[acl_l7_param.id] = acl_l7_id
+
+        return self.exec(
+            func=rp_func_name.acl_l7,
+            action=rp_action.edit,
+            param=param
+        )
+
+    def del_acl_l7(self, acl_l7_id):
+        return self.exec(
+            func=rp_func_name.acl_l7,
+            action=rp_action.delete,
+            param={
+                acl_l7_param.id: acl_l7_id,
+            }
+        )
+
+    def disable_acl_l7(self, acl_l7_id):
+        return self.exec(
+            func=rp_func_name.acl_l7,
+            action=rp_action.down,
+            param={
+                acl_l7_param.id: acl_l7_id,
+            }
+        )
+
+    def enable_acl_l7(self, acl_l7_id):
+        return self.exec(
+            func=rp_func_name.acl_l7,
+            action=rp_action.up,
+            param={
+                acl_l7_param.id: acl_l7_id,
+            }
+        )
+
+    # }}}
