38ec52838fb33a700b4e6271cb0806ebd13cdbfe
dzhuang <dzhuang.scut@gmail.com>
2024-02-14 14:21:45 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,6 @@
-import os
-
 from setuptools import find_packages, setup
 
 from src import pyikuai
-
-
-def read(fname):
-    # file read function copied from sorl.django-documents project
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
 install_requires = [
@@ -17,9 +10,9 @@ install_requires = [
 setup(
     name='pyikuai',
     version=pyikuai.__version__,
-    description="iKuai router api",
-    long_description_content_type="text/x-rst",
-    long_description=read('README.md'),
+    description="A Python client for interacting with IKuai routers",
+    long_description_content_type="text/markdown",
+    long_description=open('README.md').read(),
     classifiers=[
         # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
         'Development Status :: 3 - Alpha',
@@ -36,14 +29,15 @@ setup(
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-    keywords='iKuai Router API',
+    keywords='iKuai Router client API',
     author='Dong Zhuang',
     author_email='dzhuang.scut@gmail.com',
-    url='https://github.com/dzhuang/PYiKuaiRouter/',
+    url='https://github.com/dzhuang/PyIKuaiClient/',
     license='MIT',
     packages=find_packages("src"),
     include_package_data=True,
     zip_safe=True,
     install_requires=install_requires,
-    package_dir={"": "src"}
+    package_dir={"": "src"},
+    python_requires='>=3.6',
 )
